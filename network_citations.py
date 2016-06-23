0f87818d5e3f028f0863e01fad26b6db4bdaa302
milad <milad@miladalsh.local>
2016-06-23 17:11:05 +0200

    fs

--- network_citations.py
+++ network_citations.py
@@ -346,7 +346,7 @@ def learn_model(sc):
 		model = LinearRegressionWithSGD.train(training, iterations=iterations, step=0.1)
 		preds = testing.map(lambda p: (p.label, model.predict(p.features)))
 		MSE = preds.map(lambda r: (r[1] - r[0])**2).reduce(lambda x, y: x + y) / preds.count()
-		print("MSE = " + str(MSE))
+		print("========== MSE = " + str(MSE))
 		iterations = iterations +100
 		index = index +1
 
