8ca68b0d353b7f077f1b5d02cf221f92d9a5d804
milad <milad@miladalsh.local>
2016-06-24 19:36:04 +0200

    ddd

--- network_citations.py
+++ network_citations.py
@@ -357,7 +357,7 @@ def test(sc):
 	papers_c  = sc.textFile("/user/bd-ss16-g3/data_all/papers_citations_less_200c").map(lambda line : line.split("\t")).map(lambda c: (c[0], c[1]))
 	papers_i    = sc.textFile("/corpora/corpus-microsoft-academic-graph/data/Papers.tsv.bz2").map(lambda line : line.split("\t")).map(lambda c: (c[0], c[3]))
 	result = papers_c.join(papers_i)
-	result.saveAsHadoopFile("/user/bd-ss16-g3/data_all/papers_citations_less_200c_year")
+	result.saveAsHadoopFile("/user/bd-ss16-g3/data_all/papers_citations_less_200c_year", "org.apache.hadoop.mapred.TextOutputFormat", compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec")
 
 if __name__ == "__main__":
 	# Configure OPTIONS
