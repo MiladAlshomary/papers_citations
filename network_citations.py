b554a42902678eafec265ea7df66c6c6d2ac4ff9
milad <milad@miladalsh.local>
2016-06-25 18:32:49 +0200

    fixes

--- network_citations.py
+++ network_citations.py
@@ -405,7 +405,8 @@ def test(sc):
 	#join with papers
 	papers = sc.textFile("/user/bd-ss16-g3/data_all/papers_citations_less_200c_year").map(lambda a: a.split("\t")).map(lambda a: (a[0], float(a[1])))
 	result2 = papers.join(result).map(lambda r: (p[0], p[1][0], 0 if p[1][1] == None else p[1][1]))
-	result2.saveAsHadoopFile("/user/bd-ss16-g3/data_all/paper_author_weight_citations", "org.apache.hadoop.mapred.TextOutputFormat", compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec")
+	print(result2.take(5))
+	#result2.saveAsHadoopFile("/user/bd-ss16-g3/data_all/paper_author_weight_citations", "org.apache.hadoop.mapred.TextOutputFormat", compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec")
 
 if __name__ == "__main__":
 	# Configure OPTIONS
