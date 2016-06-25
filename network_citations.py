25a7065c2d7843fbdca349e268f2843f3dc4c209
milad <milad@miladalsh.local>
2016-06-25 15:51:52 +0200

    fixes

--- network_citations.py
+++ network_citations.py
@@ -375,13 +375,23 @@ def test(sc):
 	# c3years = citations_2.filter(lambda c: (int(c[3]) - int(c[1])) == 3).map(lambda c: (c[0], c[2]))
 	# c3years.saveAsHadoopFile("/user/bd-ss16-g3/data_all/citations_3years_old", "org.apache.hadoop.mapred.TextOutputFormat", compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec")
 
-	citations3years = sc.textFile("/user/bd-ss16-g3/data_all/citations_3years_old").map(lambda line : line.split("\t"))
-	cited_papers = citations3years.map(lambda l: (l[1], 1)).reduceByKey(lambda a,b: a+b)
-	all_papers   = sc.textFile("/user/bd-ss16-g3/data_all/papers_citations_less_200c").map(lambda line : line.split("\t")).map(lambda c: (c[0], c[1]))
-	all_papers   = all_papers.leftOuterJoin(cited_papers)
-	#update all papers nb citations from cited_papers
-	all_papers = all_papers.map(lambda p: (p[0], p[1][1])).map(lambda p: (p[0], 0 if p[1] == None else p[1]))
-	all_papers.saveAsHadoopFile("/user/bd-ss16-g3/data_all/papers_citations_less_200c_3years_citations", "org.apache.hadoop.mapred.TextOutputFormat", compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec")
+	# citations3years = sc.textFile("/user/bd-ss16-g3/data_all/citations_3years_old").map(lambda line : line.split("\t"))
+	# cited_papers = citations3years.map(lambda l: (l[1], 1)).reduceByKey(lambda a,b: a+b)
+	# all_papers   = sc.textFile("/user/bd-ss16-g3/data_all/papers_citations_less_200c").map(lambda line : line.split("\t")).map(lambda c: (c[0], c[1]))
+	# all_papers   = all_papers.leftOuterJoin(cited_papers)
+	# #update all papers nb citations from cited_papers
+	# all_papers = all_papers.map(lambda p: (p[0], p[1][1])).map(lambda p: (p[0], 0 if p[1] == None else p[1]))
+	# all_papers.saveAsHadoopFile("/user/bd-ss16-g3/data_all/papers_citations_less_200c_3years_citations", "org.apache.hadoop.mapred.TextOutputFormat", compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec")
+
+	#author feature
+	all_papers = sc.textFile("/user/bd-ss16-g3/data_all/papers_citations_less_200c_3years_citations").map(lambda p: p.split("\t")).map(lambda p: (p[0], p[1]))
+	paa = sc.textFile("/corpora/corpus-microsoft-academic-graph/data/PaperAuthorAffiliations.tsv.bz2").map(lambda l : l.split("\t")).filter(lambda a : a[1] != '')
+	paa = paa.map(lambda p: (p[0], (p[1], 1/float(l[5]))))
+	result = paa.join(all_papers)
+	result = result.map(lambda i: (i[1][0][0], 0 if i[1][1] == None else (i[1][0][1] * i[1][1]) ))
+	#reduce by combining
+	result = result.combineByKey(lambda value: (value, 1),lambda x, value: (x[0] + value, x[1] + 1),lambda x, y: (x[0] + y[0], x[1] + y[1]))
+	result.saveAsHadoopFile("/user/bd-ss16-g3/data_all/authors_weights", "org.apache.hadoop.mapred.TextOutputFormat", compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec")
 
 if __name__ == "__main__":
 	# Configure OPTIONS
