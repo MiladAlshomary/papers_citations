e87a125135873de0a65ec9f43e30efd2f850b6a7
milad <milad@miladalsh.local>
2016-06-29 11:53:58 +0200

    l

--- network_citations.py
+++ network_citations.py
@@ -528,8 +528,8 @@ if __name__ == "__main__":
 	# model = learn_model(sc, "/user/bd-ss16-g3/data_all/paper_author_weight_citations", False)
 	# model.save(sc,'/user/bd-ss16-g3/data_all/author_model')
 
-	model = learn_model(sc, "/user/bd-ss16-g3/data_all/paper_affiliations_weight_citations", False)
-	model.save(sc,'/user/bd-ss16-g3/data_all/affiliation_model')
+	model = learn_model(sc, "/user/bd-ss16-g3/data_all/paper_conf_weight_citations", False)
+	model.save(sc,'/user/bd-ss16-g3/data_all/conference_model')
 
 	#step1
 	#Extract weights for the features
